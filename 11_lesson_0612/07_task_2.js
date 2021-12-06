71a5a9e34e2745e8c58354b75f7e328677108e86
demidraksin <greatraksin@icloud.com>
2021-12-06 18:28:46 +0300

    Adding while loops

---
+++ 11_lesson_0612/07_task_2.js
@@ -0,0 +1,9 @@
+let n = 3;
+let summary = 0;
+
+for (let i = 1; i <= n; i++) {
+    summary += i;
+}
+console.log(summary)
+
+
---
+++ 11_lesson_0612/08_task_3.js
@@ -0,0 +1,9 @@
+let s = 1000;
+let n = 3;
+
+for (let i = 1; i <= n; i++) {
+    s += s * 0.03;
+}
+
+console.log(s)
+
