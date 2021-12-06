8574e76de08701c8beeae39ce032ea1a10db87bc
demidraksin <greatraksin@icloud.com>
2021-12-06 18:01:40 +0300

    Adding while loops

---
+++ 11_lesson_0612/02_do_while.js
@@ -0,0 +1,8 @@
+let x = 1;
+
+do {
+    x = x + 1;
+    console.log(x);
+} while (x < 10);
+
+
---
+++ 11_lesson_0612/05_for_vs_while.js
@@ -0,0 +1,12 @@
+let a = 10;
+while (a >= 0) {
+    console.log(a);
+    a--;
+}
+
+let a = 10;
+for (; a >= 10;) {
+    console.log(a);
+    a--
+}
+
