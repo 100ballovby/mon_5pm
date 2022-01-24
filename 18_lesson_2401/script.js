9a282ce7d2bd54121c5bea8f5526d6424a5ebad0
demidraksin <greatraksin@icloud.com>
2022-01-24 18:28:56 +0300

    Adding creating element lesson

---
+++ 18_lesson_2401/script.js
@@ -0,0 +1,5 @@
+let cardList = document.querySelector('.products')
+let listItem = document.createElement('li');  // создаю новый элемент li
+listItem.classList.add('product')
+cardList.appendChild(listItem)  // добавить наследника (дочерний элемент)
+
