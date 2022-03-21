ce1369f365a15fbd35883ed982a646b92aa3ca4b
demidraksin <greatraksin@icloud.com>
2022-03-21 18:04:47 +0300

    Finishing form sending dataa

--- 23_lesson_1403/app.py
+++ 23_lesson_1403/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '593d29bc26b00cc468c5e93299602626852422a48cf3489c'
 
 messages = [
     {'title': 'Message One',
@@ -17,6 +18,17 @@ def index():
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
+    if request.method == 'POST':  # если форма отправляется
+        title = request.form['title']  # обращаюсь к полю title из формы и забираю информацию оттуда в переменную
+        message = request.form['content']
+
+        if not title:  # если не заполнили заголовок сообщения
+            flash('Title is required.')  # вывести сообщение об этом
+        elif not message:  # если не заполнили поле с текстом сообщения
+            flash('Message is required.')  # вывести сообщение об этом
+        else:  # иначе
+            messages.append({'title': title, 'content': message})  # добавить сообщение в список
+            return redirect(url_for('index'))  # перенаправить пользователя на главную
     return render_template('create.html')
 
 
